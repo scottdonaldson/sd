59b8e748e4d3c302775bb00891bd2f91ac4e80e5
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-18 23:10:06 -0600

    more portfolio changes

--- templates/portfolio.php
+++ templates/portfolio.php
@@ -4,34 +4,39 @@ Template Name: Portfolio
 */
 get_header(); the_post(); ?>
 
-	<div class="primary">
+    <div class="primary">
 
-		<article <?php post_class(); ?>>
+        <article <?php post_class(); ?>>
                         
-			<h1 class="entry-title">
-            	Web Development Portfolio
+            <h1 class="entry-title visuallyhidden">
+                Web Development Portfolio
             </h1>
             
             <div class="author visuallyhidden">By <?php the_author(); ?></div>
             
-			<div class="entry-content clearfix">
-				<?php
+            <div class="entry-content clearfix">
+                <?php
                 $items = get_field('items');
                 $p = 1;
                 
                 foreach ($items as $item) { ?>
                     <a class="item p-<?php echo $p; ?>" href="<?php echo $item['link']; ?>" target="_blank">
-                        <h2><?php echo $item['name']; ?></h2>
                         <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['name']; ?>">
-                        <span class="visit">Visit Site</a>
+                        <div class="description">
+                            <h2><?php echo $item['name']; ?></h2>
+                            <p><?php echo $item['description']; ?></p>
+                        </div>
+                        <span class="league">Visit Site</a>
                     </a>                
                 <?php 
                 $p++;
                 } ?>
-			</div>   
+            </div>
+
+            <a href="<?php echo home_url(); ?>/blog/category/web" class="web league">Read my blog posts on web dev</a>
             
-		</article><!-- .post -->
+        </article><!-- .post -->
             
-	</div>                
+    </div>                
 
 <?php get_footer(); ?>
