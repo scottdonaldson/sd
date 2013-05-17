5f441b09c135bd66dbb854c347f5cce6133acb05
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-05-17 15:48:49 -0400

    update to jquery 1.9.1

--- footer.php
+++ footer.php
@@ -22,8 +22,8 @@
 
 </div><!-- #page .hfeed .container -->
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo bloginfo('template_url'); ?>/js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo bloginfo('template_url'); ?>/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 	<script src="<?php echo bloginfo('template_url'); ?>/js/plugins.js"></script>
     <script src="<?php echo bloginfo('template_url'); ?>/js/script.js"></script>
 
--- js/scrobbler.js
+++ js/scrobbler.js
@@ -3,9 +3,9 @@ jQuery(document).ready(function($){
 	function getStream() {
 		$.ajax({
 			url: 'http://www.thecurrent.org/playlist/metadata/current',
-			dataType: 'jsonp',
+			dataType: 'text/html',
 			success: function(data){
-				console.log(data);
+				console.log(data.toString());
 			}
 		})
 	}
