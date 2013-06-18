35e4789fb3d4e05495169144d6363965408ece72
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-06-18 16:14:00 -0400

    more unzipping

--- js/unzip.js
+++ js/unzip.js
@@ -1,28 +1,108 @@
 jQuery(document).ready(function($){
 
+	var body = $('body');
+
 	// User inputs
 	var input = $('#zip'),
 		zips,
 		output_cities = $('#city'),
 		output_states = $('#state'),
-		cities = [],
-		states = [];
+		error = $('#error'),
+		theError,
+		expand = $('.expand');
 
 	var url = 'http://zip.elevenbasetwo.com/v2/US/';
+	
+	function returnError(i) {
+		output_cities.val(output_cities.val() + 'ERROR\n');
+		output_states.val(output_states.val() + 'ERROR\n');
 
-	function unzip() {
-		zips = input.val().split('\n');
+		error.html('Error on line ' + (i + 1) + '. Check the list of ZIP codes.');
 
-		for (var i = 0; i < zips.length; i++) {
+		theError = i;
+		expand.addClass('shown').on('click', function(){
+			explode(theError);
+		});
+	}
+
+	function returnSuccess(data) {
+		output_cities.val(output_cities.val() + data.city + '\n');
+		output_states.val(output_states.val() + data.state + '\n');
+	}
+
+	function clearOutput() {
+		error.html('');
+		output_cities.val('');
+		output_states.val('');
+	}
+
+	function getResult(zips, i) {
+		if (i < zips.length) {
 			$.ajax({
 				url: url + zips[i],
+				error: function(){
+					returnError(i);
+				},
 				success: function(data) {
-					output_cities.val(output_cities.val() + data.city + '\n');
-					output_states.val(output_states.val() + data.state + '\n');
+					returnSuccess(data);
+					getResult(zips, i + 1);
 				}
 			});
 		}
 	}
+
+	function unzip() {
+		clearOutput();
+		zips = input.val().split('\n');
+
+		getResult(zips, 0);
+	}
 	$('input[type="submit"]').on('click', unzip);
+
+	var overlay = $('<div class="overlay"></div>'),
+		checkInput = $('<div class="check-input"></div>'),
+		checking,
+		errorStart;
+	function explode(theError) {
+
+		overlay.appendTo(body).css({
+			opacity: 1
+		});
+
+		setTimeout(function(){
+			checkInput.appendTo(body).html(input.val().replace(/\n/g, '<br>'));
+
+			checking = checkInput.html().split('<br>');
+			
+			for (var i = 0; i < checking.length; i++) {
+				checkInput.prepend('<span class="indicator" style="top: ' + (40 + (22 * i)) + 'px;">' + (i + 1) + '.</span>');
+			}
+
+			// we can use indexOf() since we're looking for the first instance of 
+			// the thing that threw the error
+			errorStart = checkInput.html().indexOf(checking[theError]);
+			console.log(checkInput.html().substr(errorStart, checking[theError].length));
+			checkInput.html(checkInput.html().replace(checkInput.html().substr(errorStart, checking[theError].length), '<span style="background: #ff0;">' + checkInput.html().substr(errorStart, checking[theError].length) + '</span>'));
+
+		}, 500);
+	}
+
+	function resetErrors() {
+		checkInput.remove();
+		overlay.css({
+			opacity: 0
+		});
+		setTimeout(function(){
+			overlay.remove();
+		}, 500);
+
+		expand.removeClass('shown');
+	}
+
+	$(window).on('keydown', function(e) {
+		if (e.keyCode === 27) {
+			resetErrors();
+		}
+	});
 
 });
--- templates/unzip.php
+++ templates/unzip.php
@@ -3,17 +3,12 @@
 Template Name: Unzip
 */
 
-function social_css() { ?>
-	<link rel="stylesheet" href="<?php echo bloginfo('template_url'); ?>/css/social.css">
+function unzip_css() { ?>
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url'); ?>/css/unzip.css">
 <?php }
-add_action('wp_head', 'social_css');
+add_action('wp_head', 'unzip_css');
 
 get_header(); ?>
-<style>
-	header {
-		display: none;
-	}
-</style>
 <?php
 the_post(); 
 ?>
@@ -22,8 +17,14 @@ the_post();
 
 		<article <?php post_class(); ?>>
 
+			<h1 class="entry-title">
+            	<?php the_title(); ?>
+            </h1>
+            <p>A utility for converting ZIP codes to cities and states (United States only). Copy a list of ZIP codes, one per line, and paste into the box on the left. After unzipping, the list of cities and states will populate in the boxes on the right.</p>
+
 			<div class="input">
 				<textarea name="zip" id="zip"></textarea>
+				<div class="expand">+</div>
 			</div>
 
 			<div class="output">
@@ -33,6 +34,8 @@ the_post();
 			<div class="output">
 				<textarea name="unzip" id="state"></textarea>
 			</div>
+
+			<div id="error"></div>
 
 			<input type="submit" value="Unzip">
 
