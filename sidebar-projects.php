55b8df9be1bf3b62ea388f7fba0d2b49a7b5085c
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-09-07 16:42:26 -0400

    P&S based out of DC, not MSP (also PHP shortcodes)

--- sidebar-projects.php
+++ sidebar-projects.php
@@ -4,25 +4,25 @@
 
         <a href="http://www.restho.me" target="_blank">
             <h4>'Rest Home</h4>
-            <img src="<?php echo bloginfo('template_url'); ?>/images/RestHome-photo.jpg" alt="'Rest Home" />
+            <img src="<?= bloginfo('template_url'); ?>/images/RestHome-photo.jpg" alt="'Rest Home" />
         </a>
         <p>Internet comedy series written by and starring Tim Carroll, Scott Donaldson, and Greg Hunter.</p>
         
         <a href="http://www.parsleyandsprouts.com" target="_blank">
         	<h4>Parsley &amp; Sprouts</h4>
-        	<img src="<?php echo bloginfo('template_url'); ?>/images/Parsley-photo.jpg" alt="Parsley and Sprouts" />
+        	<img src="<?= bloginfo('template_url'); ?>/images/Parsley-photo.jpg" alt="Parsley and Sprouts" />
         </a>
-        <p>Minneapolis and St. Paul web and print design, code, and development.</p>  
+        <p>Washington DC-based web and print design, code, and development (formerly located in Minneapolis/St. Paul).</p>  
 
-        <a href="<?php echo home_url(); ?>/st-paul-skyways">
+        <a href="<?= home_url(); ?>/st-paul-skyways">
             <h4>St. Paul Skyways</h4>
-            <img src="<?php echo bloginfo('template_url'); ?>/images/skyway-thumbnail.jpg" alt="St. Paul Skyways">
+            <img src="<?= bloginfo('template_url'); ?>/images/skyway-thumbnail.jpg" alt="St. Paul Skyways">
         </a>
         <p>An ongoing project to draw every Skyway in downtown St. Paul.</p>
               
         <a href="http://www.playgroundmisnomer.com" target="_blank">
             <h4>Playground Misnomer</h4>
-            <img src="<?php echo bloginfo('template_url'); ?>/images/PM-photo.jpg" alt="Playground Misnomer" />
+            <img src="<?= bloginfo('template_url'); ?>/images/PM-photo.jpg" alt="Playground Misnomer" />
         </a>
         
         <p>A Midwest music blog, with album and concert reviews of national and local artists.</p>
