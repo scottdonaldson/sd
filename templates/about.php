d00378933504d398651afd508ee38d11de0edd04
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-07-06 23:19:35 -0400

    updated photo on about pg (thanks Tom!)

--- templates/about.php
+++ templates/about.php
@@ -25,7 +25,7 @@ get_header(); ?>
 					?>
                     
 					<div class="entry-content">
-						<img class="aligncenter size-full" title="Scott Donaldson. Photo by Nate Ryan." alt="Scott Donaldson. Photo by Nate Ryan." src="http://scottdonaldson.net/wp-content/uploads/2011/11/Scott-Donaldson.jpg">
+						<img class="aligncenter size-full" title="Scott Donaldson. Photo by Thomas Dunn." alt="Scott Donaldson. Photo by Nate Ryan." src="http://scottdonaldson.net/wp-content/uploads/2014/07/Scott-Donaldson-2014-1000x800.jpg">
 
 						<p>I'm a DC-based front-end web developer and caretaker of two bunnies. With designer Lisa Otto, I'm one half of the web team <a href="http://www.parsleyandsprouts.com" target="_blank">Parsley &amp; Sprouts</a>. I'm a cowriter and performer of the Internet comedy series <a href="http://www.restho.me" target="_blank">'Rest Home</a>. You might also catch me writing for <a href="http://www.playgroundmisnomer.com/author/scott" target="_blank">Playground Misnomer</a>, a Midwest music blog. I'm <?= $days ?> days old. This site is for my other doings, thinkings, and makings. You might find them interesting also.</p>
 						<p>Email me at <a href="mailto:scott.p.donaldson@gmail.com">scott.p.donaldson@gmail.com</a>.</p>
